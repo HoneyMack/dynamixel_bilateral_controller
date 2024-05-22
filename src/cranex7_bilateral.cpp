401fcd37cac36abf2a4bf9e1955f8e6eab826400
HoneyMack <tosimiti7@gmail.com>
2024-05-22 03:11:28 +0000

    パラメータの調整と制御周期を出力するように修正

--- src/cranex7_bilateral.cpp
+++ src/cranex7_bilateral.cpp
@@ -19,6 +19,7 @@
 #include <atomic>
 #include <chrono>
 #include <functional>
+#include <algorithm>
 
 #include <dynamixel_sdk/dynamixel_sdk.h> // Uses DYNAMIXEL SDK library
 #include <unistd.h>
@@ -132,10 +133,10 @@ int main() {
 
     //疑似微分器
     // double T_control = 1.0 / 500;
-    double T_control = 1.0 / 335;
+    double T_control = 1.0 / 500;
     double cutoff_diff = 20;
-    double cutoff_disturbance = 2.0;
-    double cutoff_reaction = 2.0;
+    double cutoff_disturbance = 0.5;
+    double cutoff_reaction = 0.5;
 
     //cranex7 observer のパラメータ
     // TODO: 要パラメータ調整
@@ -182,11 +183,11 @@ int main() {
         {5, 289.0},
         {6, 324.0},
         {7, 144.0},
-        {8, 324.0}
+        {8, 324.0*4}
     };
 
     map<int, double> Kds = {
-        {1, 40.0 / 1.5},
+        {1, 40.0},
         {2, 28.0},
         // {3, 66.0},
         {4, 24.0},
@@ -210,14 +211,14 @@ int main() {
     //     {8, 1.00},
     // };
     map<int, double> Kts = {
-        {1, 0.70},
-        {2, 0.70},
+        {1, 1.00},
+        {2, 1.00},
         //     {3, 1.00},
-            {4, 0.70},
+            {4, 0.90},
             //{5, 0.80}, //ここをオンにするとバイラテ制御がうまくいかない
             //{6, 0.80},
             //{7, 0.80},
-            {8, 0.80},
+            {8, 1.0},
     };
 
     //角度の単位変換に伴うパラメータの変換
@@ -242,7 +243,10 @@ int main() {
     atomic<bool> finish_flag(false), start_get_l_state_flag(false), start_get_f_state_flag(false);
     map<int, double> vel_l, vel_f, pos_l, pos_f;
     //実行時間計測
+    const int mean_data_num = 100;
     chrono::system_clock::time_point time_start, time_end;
+    //分散計算用
+    vector<double> elapsed_times(mean_data_num, 0.0);
 
     time_start = chrono::system_clock::now();
     function<void()> get_l_state = [&]() {
@@ -315,17 +319,29 @@ int main() {
             torque_goal_l = convert_joint_to_dxl_idx(torque_goal_l);
             torque_goal_f = convert_joint_to_dxl_idx(torque_goal_f);
 
+            // トルクが入力範囲内に収まるようにclip
+            torque_goal_l = leaderDxlHandler.clipTorques(torque_goal_l);
+            torque_goal_f = followerDxlHandler.clipTorques(torque_goal_f);
+
             leaderDxlHandler.setTorques(torque_goal_l);
             followerDxlHandler.setTorques(torque_goal_f);
 
+            time_end = chrono::system_clock::now();
+            double elapsed = chrono::duration_cast<chrono::milliseconds>(time_end - time_start).count();
+            elapsed_times[counter] = elapsed;
             counter++;
-            if (counter >= 100) {
-                // 周波数を表示
-                time_end = chrono::system_clock::now();
-                double elapsed = chrono::duration_cast<chrono::milliseconds>(time_end - time_start).count();
-                double freq = counter / (elapsed / 1000);
-                printf("freq: %5.3f\n", freq);
 
+            if (counter >= mean_data_num) {
+                // 周波数・制御周期を表示
+                double interval_average = 0.0, interval_std = 0.0;
+                for (const auto& interval : elapsed_times) {
+                    interval_average += interval;
+                    interval_std += interval * interval;
+                }
+                interval_average /= mean_data_num;
+                interval_std = sqrt(interval_std / mean_data_num - interval_average * interval_average);
+                printf("interval: %5.3f +- %5.3f [ms]\n", interval_average, interval_std);
+                printf("freq: %5.3f +- %5.3f\n", 1000.0 / interval_average, 1000.0 * interval_std/(interval_average*interval_average));
                 // 現在の状態を表示
                 //torque
                 printf("torque:\t");
@@ -348,14 +364,12 @@ int main() {
                 }
                 printf("\n\n\n");
                 counter = 0;
-
-                time_start = chrono::system_clock::now();
-
             }
             // // 現在の状態を表示
             // for (const auto& id : dxlHandler.dxl_ids) {
             //     printf("ID: %d, Cur: %4f mA, Vel: %4f, Pos: %4f, tau_d:%4f, tau_r:%4f \n", id, goal_currents[id], velocities[id], positions[id], tau_d[id], tau_r[id]);
             // }
+            time_start = chrono::system_clock::now();
         }
         };
     thread get_l_state_thread(get_l_state), get_f_state_thread(get_f_state);
